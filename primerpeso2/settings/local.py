4430d5655cea03683e11d80f1f01224c2480b1b3
Mikela Clemmons <glassresistor@gmail.com>
2016-02-11 15:49:11 -0800

    Setup settings files to work with ansible

--- primerpeso2/settings/local.py
+++ primerpeso2/settings/local.py
@@ -1,7 +1,7 @@
 from primerpeso2.settings.base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'change me'
+SECRET_KEY = 'sq)eck#=xcu13k0%aw)11dpqbf6hd@$e5kjel*qk2i92yckn+*'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
@@ -9,6 +9,8 @@ SECRET_KEY = 'change me'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'primerpeso2',
+        'NAME': 'primerpeso',
+        'USER': 'password',
+        'HOST': 'localhost',
     }
 }
