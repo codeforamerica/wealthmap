73902262d6c0238d2463dfa362102e5a9b816bab
Mikela Clemmons <glassresistor@gmail.com>
2016-02-18 12:07:00 -0800

    Testing DATABASE_HOST from environment

---
+++ primerpeso2/settings/environment.py
@@ -0,0 +1,21 @@
+import os
+from primerpeso2.settings.base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'primerpeso2',
+        'USER': os.environ['DATABASE_USER'],
+        'HOST': os.environ['DATBASE_HOST',
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+    }
+}
+
+STATIC_ROOT = os.environ['STATIC_ROOT']
+MEDIA_ROOT = os.environ['MEDIA_ROOT']
--- primerpeso2/settings/local.py
+++ primerpeso2/settings/local.py
@@ -1,20 +1,3 @@
-import os
-from primerpeso2.settings.base import *
+from primerpeso2.settings.environment import *
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sq)eck#=xcu13k0%aw)11dpqbf6hd@$e5kjel*qk2i92yckn+*'
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'primerpeso2',
-        'USER': os.environ['DATABASE_USER'],
-        'HOST': 'localhost',
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-    }
-}
-
-STATIC_ROOT = os.environ['STATIC_ROOT']
