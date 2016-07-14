652ae73232d3eb2a72ef908c7238fcdee44aae33
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-07-14 10:59:16 -0700

    Added missing Industry to search serializer

--- wealthmap/serializers.py
+++ wealthmap/serializers.py
@@ -41,6 +41,8 @@ class OpportunitySerializer(serializers.ModelSerializer):
 
 class OpportunitySearchSerializer(serializers.ModelSerializer):
     results = serializers.SerializerMethodField()
+    industry = serializers.PrimaryKeyRelatedField(
+        queryset=models.Industry.objects.all())
     purposes = serializers.PrimaryKeyRelatedField(
         queryset=models.Purpose.objects.all(), many=True)
 
